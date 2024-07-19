dd5d67a4f657ebcbdbd3f443687a6b178ba7fc30
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-07-18 21:17:28 -0600

    orden

--- Backend/Munaskay/js/index.js
+++
@@ -1,7 +0,0 @@
-// MESERO DIME QUIEN ES EL AMOR DE MI VIDA
-
-fetch("http://127.0.0.1:5000")
-.then(r => r.json())
-.then(r => {
-    console.log(r);
-})
--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -13,3 +13,37 @@ btnSignUp.addEventListener("click", e => {
     containerFormLogin.classList.add("hide");
     containerFormRegister.classList.remove("hide")
 })
+
+document.getElementById('form_login')
+.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    let formData = {}
+
+    for (const element of event.target.elements) {
+        if (element.name && !element.file) {
+            formData[element.name] = element.value
+        }
+    }
+
+    fetch("http://localhost:5000/api/v1/auth/login", {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+            'Content-type': "application/json"
+        }
+    })
+    .then(r => r.json())
+    .then(r => {
+
+        if(r.error) {
+            console.log(r.message);
+        } else {
+            window.location.href = '/perfil.html'
+        }
+ 
+    })
+    .catch(r => {
+        console.log(r);
+    })
+})
